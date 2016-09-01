12bcf5255a916ab0a88c007adaedeffddfade257
sguzunov <sguzunov@gmail.com>
2016-09-01 14:16:20 +0300

    Partially added hw for HQC - Part 1 Naming Identifiers.

---
+++ HQC - Part 1/Naming Identifiers/People/People/PersonFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace People
+{
+    public class PersonFactory
+    {
+        public Person CreatePerson(int age)
+        {
+            var person = new Person()
+            {
+                Age = age
+            };
+
+            if (age % 2 == 0)
+            {
+                person.Name = "Bad boy";
+                person.Gender = Gender.Male;
+            }
+            else
+            {
+                person.Name = "Hot chick";
+                person.Gender = Gender.Female;
+            }
+
+            return person;
+        }
+    }
+}
