5c54eb0a8a26e9befeeb276fdc2f3bfb5bea8ad4
sguzunov <sguzunov@gmail.com>
2016-11-19 23:21:58 +0200

    Added task for Labyrinth N x N.

---
+++ DSA/Linear Data Structures - Homework/Linear Data Structures/LabyrinthNxN/Position.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace LabyrinthNxN
+{
+    public class Position
+    {
+        public Position()
+        {
+            this.Neighbours = new List<Position>();
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public ICollection<Position> Neighbours;
+    }
+}
---
+++ DSA/Linear Data Structures - Homework/Linear Data Structures/LabyrinthNxN/Startup.cs
@@ -0,0 +1,99 @@
+﻿// We are given a labyrinth of size N x N.
+// Some of its cells are empty(0) and some are full(x).
+// We can move from an empty cell to another empty cell if they share common wall.
+// Given a starting position (*) calculate and fill in the array the minimal distance from this position to any other cell in the array. Use "u" for all unreachable cells
+
+using System;
+using System.Collections.Generic;
+
+namespace LabyrinthNxN
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var matrix = new string[,]
+            {
+                {"0","0","0","x","0","x" },
+                {"0","x","0","x","0","x" },
+                {"0","*","x","0","x","0" },
+                {"0","x","0","0","0","0" },
+                {"0","0","0","x","x","0" },
+                {"0","0","0","x","0","x" }
+            };
+
+            var startPosition = new Position() { X = 1, Y = 2 };
+            AddMinimalDistanceFromPosition(startPosition, matrix);
+            MarkUnreachable(matrix);
+
+            Print(matrix);
+        }
+
+        private static void MarkUnreachable(string[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == "0")
+                    {
+                        matrix[i, j] = "u";
+                    }
+                }
+            }
+        }
+
+        private static void Print(string[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void AddMinimalDistanceFromPosition(Position start, string[,] matrix)
+        {
+            var positions = new Queue<Position>();
+            positions.Enqueue(start);
+            AddNeighbours(start, matrix);
+            while (positions.Count > 0)
+            {
+                var position = positions.Dequeue();
+                foreach (var neighbour in position.Neighbours)
+                {
+                    int distance = 1;
+                    int.TryParse(matrix[position.Y, position.X], out distance);
+                    distance++;
+                    matrix[neighbour.Y, neighbour.X] = distance.ToString();
+
+                    AddNeighbours(neighbour, matrix);
+                    positions.Enqueue(neighbour);
+                }
+            }
+        }
+
+        private static void AddNeighbours(Position position, string[,] matrix)
+        {
+            var deltasX = new int[] { 0, 1, 0, -1 };
+            var deltasY = new int[] { -1, 0, 1, 0 };
+
+            for (int i = 0; i < deltasX.Length; i++)
+            {
+                int nextY = position.Y + deltasY[i];
+                int nextX = position.X + deltasX[i];
+
+                bool inRange = (0 <= nextX && nextX < matrix.GetLength(1)) &&
+                    (0 <= nextY && nextY < matrix.GetLength(0));
+                if (inRange && matrix[nextY, nextX] == "0")
+                {
+                    position.Neighbours.Add(new Position { X = nextX, Y = nextY });
+                }
+            }
+        }
+    }
+}
