66c3f7726d17f159a28ec8899da258dd1533c9ca
sguzunov <sguzunov@gmail.com>
2016-11-24 20:45:46 +0200

    Started doing recursion homework.

---
+++ DSA/Recursion - Homework/Recursion/CombinationsWithDuplicates/Startup.cs
@@ -0,0 +1,35 @@
+﻿// Write a recursive program for generating and printing all the combinations WITH duplicatesof k elements from n-element set.
+// n=3, k=2 → (1 1), (1 2), (1 3), (2 2), (2 3), (3 3)
+
+using System;
+
+namespace CombinationsWithDuplicates
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            Console.Write("K elements: ");
+            int k = int.Parse(Console.ReadLine());
+            Console.Write("N-element set: ");
+            int n = int.Parse(Console.ReadLine());
+
+            GenerateCombinationsWithDuplicates(0, 1, k, n, new int[k]);
+        }
+
+        private static void GenerateCombinationsWithDuplicates(int index, int start, int k, int n, int[] combinations)
+        {
+            if (k <= index)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = start; i <= n; i++)
+            {
+                combinations[index] = i;
+                GenerateCombinationsWithDuplicates(index + 1, i, k, n, combinations);
+            }
+        }
+    }
+}
---
+++ DSA/Recursion - Homework/Recursion/NestedLoops/Startup.cs
@@ -0,0 +1,33 @@
+﻿// Write a recursive program that simulates the execution of n nested loopsfrom 1 to n.
+
+using System;
+
+namespace NestedLoops
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            Console.Write("Numebrs of loops: ");
+            int numberOfLoops = int.Parse(Console.ReadLine());
+            int[] numbers = new int[numberOfLoops];
+
+            Loop(0, numberOfLoops, numbers);
+        }
+
+        private static void Loop(int index, int n, int[] numbers)
+        {
+            if (n <= index)
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                numbers[index] = i;
+                Loop(index + 1, n, numbers);
+            }
+        }
+    }
+}
