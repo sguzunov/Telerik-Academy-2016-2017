931a52f330f8512f16d3dcc5fca947441a3d1b96
sguzunov <sguzunov@gmail.com>
2016-12-01 22:41:20 +0200

    Added hw for Combinatorics.

---
+++ DSA/Combinatorics/Combinatorics/BinaryPasswords/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BinaryPasswords
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            int stars = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '*') stars++;
+            }
+
+            ulong numberOfPasswords = 1U;
+            for (int i = 0; i < stars; i++)
+            {
+                numberOfPasswords *= 2U;
+            }
+
+            Console.WriteLine(numberOfPasswords);
+        }
+    }
+}
---
+++ DSA/Combinatorics/Combinatorics/ColoredBunnies/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ColoredBunnies
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int numberOfBunnies = int.Parse(Console.ReadLine());
+
+            var answers = new Dictionary<int, int>();
+            for (int i = 0; i < numberOfBunnies; i++)
+            {
+                int answer = int.Parse(Console.ReadLine());
+                answer++;
+                if (!answers.ContainsKey(answer))
+                {
+                    answers.Add(answer, 1);
+                }
+                else
+                {
+                    answers[answer]++;
+                }
+            }
+
+            long result = 0L;
+            foreach (var item in answers)
+            {
+                int askedBunnies = item.Value;
+                int bunnyAnswer = item.Key;
+                int groups = (int)Math.Ceiling(askedBunnies / (double)bunnyAnswer);
+                result += (long)(groups * bunnyAnswer);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
